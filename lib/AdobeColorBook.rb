1cb04f522133d236664120581fc195480e72e6df
Pat O'Neill <Pat.Ryan.Oneill@gmail.com>
2014-07-31 19:23:31 -0400

    Refactoring, moved read_string and read_int to Module, ColorBookColor start

--- lib/AdobeColorBook.rb
+++ lib/AdobeColorBook.rb
@@ -5,16 +5,47 @@ require "AdobeColorBook/version"
 module AdobeColorBook
   SIGNATURE = '8BCB'
   SIGNATURE_LENGTH = 4
-  IDENTIFIER_RGB = 0
-  IDENTIFER_CMYK = 2
-  IDENTIFIER_LAB = 7
+# header byte info
   CHAR_TRADEMARK = '^C'
   CHAR_REGISTERED = '^R'
   HEADER_BLUEPRINT = "nn"
   HEADER_LENGTH = 4
-  STRING_LENGTH = 4
+  STRING32 = 4
+  INT16 = 2
+# Color byte info
+  IDENTIFIER_RGB = 0
+  IDENTIFER_CMYK = 2
+  IDENTIFIER_LAB = 7
+  BYTES_CMYK = 4
+  BYTES_RGB, BYTES_LAB = 3
+
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def read_string (colorbook = nil)
+      # is this an instance?
+      colorbook = (instance_variable_get("@colorbook_file").nil?) ? colorbook : @colorbook_file
+      # Strings are variable, get the length, then the actual string. UTF-16, Not null-terminated
+      while len = colorbook.read(STRING32)
+        len = len.unpack('N').shift * 2  # *2 because length is 16-bit wide,
+        puts len
+        return record = colorbook.read(len)
+      end
+    end
+
+    def read_int (colorbook = nil)
+      # is this an instance?
+      colorbook = (instance_variable_get("@colorbook_file").nil?) ? colorbook : @colorbook_file
+      while len = colorbook.read(INT16)
+        return len.unpack('n').shift
+      end
+    end
+  end
 
   class ColorBook
+    include(AdobeColorBook::ClassMethods)
   # Name of color is found by combining prefix, name and suffix: (PANTONE)( 763)(U)
   attr_accessor :version, :title, :identifier, :prefix, :suffix, :description,
                 :color_count,  # number of color records in the book, MUST match the actual color count.
@@ -41,18 +72,7 @@ module AdobeColorBook
 
   private
 
-    def read_string
-      # Strings are variable, get the length, then the actual string. UTF-16, Not null-terminated
-      while len = @colorbook_file.read(STRING_LENGTH)
-        len = len.unpack('N').shift * 2  # *2 because length is 16-bit wide,
-        puts len
-        return record = @colorbook_file.read(len)
-      end
-    end
 
-    def read_int
-      @colorbook_file.read(2).unpack('n').shift
-    end
 
     def read_header
       sig = @colorbook_file.read(SIGNATURE_LENGTH)
@@ -72,11 +92,35 @@ module AdobeColorBook
 
     end
 
+    def find_color_space
+      AdobeColorBook.constants.select { |c| AdobeColorBook.const_get(c) == @color_space }.shift.to_s.split('_')[1]
+    end
+
     def read_colors
+      bytes_len = AdobeColorBook.const_get("BYTES_#{find_color_space}")
       #  TODO: Setup BookColor Class
       #  reading record: String for name, 6 bytes for short name, 3-4 bytes for color itself
     end
   end
+
+  class ColorBookColor
+    attr_accessor :name, :shortname, :color_values
+    def initialize(color_opts = nil)
+      @name = color_opts[:name]
+      @shortname = color_opts[:shortname]
+      @color_values = color_opts[:color_values]
+    end
+
+    private
+
+    def read(file_ref)
+      fail("File is not valid") unless File.file? file_ref
+      @name = read_string file_ref
+      @shortname = file_ref.read(6).unpack("cccccc")
+
+    end
+  end
+
 end # end Module
 
 def testit

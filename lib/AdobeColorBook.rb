34d2c4748689077967ca9019c40194f03fe2e50b
Pat O'Neill <Pat.Ryan.Oneill@gmail.com>
2014-08-02 22:03:23 -0400

    Major refactoring.

--- lib/AdobeColorBook.rb
+++ lib/AdobeColorBook.rb
@@ -11,60 +11,61 @@ module AdobeColorBook
   HEADER_BLUEPRINT = "nn"
   HEADER_LENGTH = 4
   STRING32 = 4
+  INT32 = 4
   INT16 = 2
+  INT8 = 1
 # Color byte info
   IDENTIFIER_RGB = 0
-  IDENTIFER_CMYK = 2
+  IDENTIFIER_CMYK = 2
   IDENTIFIER_LAB = 7
   BYTES_CMYK = 4
   BYTES_RGB, BYTES_LAB = 3
 
   def self.included(base)
-    base.extend(ClassMethods)
+    base.extend(Readers)
   end
 
-  module ClassMethods
-    def read_string (colorbook = nil)
-      # is this an instance?
-      colorbook = (instance_variable_get("@colorbook_file").nil?) ? colorbook : @colorbook_file
+  module Readers
+    def read_string
       # Strings are variable, get the length, then the actual string. UTF-16, Not null-terminated
-      while len = colorbook.read(STRING32)
-        len = len.unpack('N').shift * 2  # *2 because length is 16-bit wide,
+      while len = @colorbook_file.read(STRING32)
+        len = len.unpack('N').shift * 2 # *2 because length is 16-bit wide,
         puts len
-        return record = colorbook.read(len)
+        return record = @colorbook_file.read(len).encode('UTF-8').unpack('U')
       end
     end
 
-    def read_int (colorbook = nil)
-      # is this an instance?
-      colorbook = (instance_variable_get("@colorbook_file").nil?) ? colorbook : @colorbook_file
-      while len = colorbook.read(INT16)
+    def read_int(size = nil?)
+      size ||= INT16
+      while len = @colorbook_file.read(size)
         return len.unpack('n').shift
       end
     end
   end
 
   class ColorBook
-    include(AdobeColorBook::ClassMethods)
+  include(AdobeColorBook::Readers)
   # Name of color is found by combining prefix, name and suffix: (PANTONE)( 763)(U)
-  attr_accessor :version, :title, :identifier, :prefix, :suffix, :description,
-                :color_count,  # number of color records in the book, MUST match the actual color count.
-                :page_size, :page_selector_offset, # Photoshop UI information - size is number of colors per page, offset is WHICH color to show on page down
-                :color_space,
-                :colorbook_file, # store the file object to read and write
-                :colors
+  attr_accessor :colorbook_file, :colorbook_options # store the file object to read and write
 
-    def initialize(colorbook_file = nil)
-      #TODO: Refactor this to allow for nil entry to create a new book
-      @colors = []
-      unless colorbook_file.nil?
-      @colorbook_file = colorbook_file
-        File.open(@colorbook_file, 'rb' ) { |cbf| @colorbook_file = cbf
+    AdobeColorBook.constants.select { |cn| cn.to_s.include? 'IDENTIFIER' }.each do |c|
+      c = c.to_s.split('_')[1]
+      define_method "is_#{c.downcase}?" do
+        @colorbook_options[:color_space_value] == c
+      end
+    end
+
+    def initialize(colorbook_file, colorbook_options = {})
+      @colorbook_options = colorbook_options
+      @colorbook_file = File.new colorbook_file if (colorbook_file.nil? || File.file?(colorbook_file))
+      @colorbook_options[:colors] ||= []
+
+        File.open(@colorbook_file, 'rb:UTF-16LE' ) { |cbf| @colorbook_file = cbf
           read_header
           read_colors
         }
+        puts self
       end
-    end
 
     def to_s
       instance_variables.each { |v| puts "#{v}: #{instance_variable_get(v)}"  }
@@ -76,55 +77,63 @@ module AdobeColorBook
 
     def read_header
       sig = @colorbook_file.read(SIGNATURE_LENGTH)
-      @version, @identifier  = @colorbook_file.read(HEADER_LENGTH).unpack(HEADER_BLUEPRINT)
-      puts "#{sig}, #{@version}, #{@identifier}"
       fail("This is not a valid colorbook file") unless sig == SIGNATURE
-      @title = read_string
-      @prefix = read_string
-      @suffix = read_string
-      @description = read_string
-      @color_count = read_int
-      @page_size = read_int
-      @page_selector_offset = read_int
-      @color_space = read_int
-      @test = read_int
-      puts self
 
+      @colorbook_options[:version] = read_int
+      @colorbook_options[:identifier] = read_int
+      puts "#{sig}, #{@colorbook_options[:version]}, #{@colorbook_options[:identifier]}"
+      @colorbook_options[:title] = read_string
+      @colorbook_options[:prefix] = read_string
+      @colorbook_options[:suffix] = read_string
+      @colorbook_options[:description] = read_string
+      @colorbook_options[:color_count] = read_int
+      @colorbook_options[:page_size] = read_int
+      @colorbook_options[:page_selector_offset] = read_int
+      @colorbook_options[:color_space_value] = read_int
+      @colorbook_options[:color_space_name] = find_color_space_string
+      puts @colorbook_options
     end
 
-    def find_color_space
-      AdobeColorBook.constants.select { |c| AdobeColorBook.const_get(c) == @color_space }.shift.to_s.split('_')[1]
+    def get_colors
+      color_hash = {}
+      @colorbook_options[:color_space_name].chars.map { |c| color_hash["#{c}".to_sym]  }
     end
+
+    def find_color_space_string
+     AdobeColorBook.constants.select { |c| AdobeColorBook.const_get(c) == @colorbook_options[:color_space_value] }.shift.to_s.split('_')[1]
+    end
+
+
 
     def read_colors
-      bytes_len = AdobeColorBook.const_get("BYTES_#{find_color_space}")
-      #  TODO: Setup BookColor Class
-      #  reading record: String for name, 6 bytes for short name, 3-4 bytes for color itself
+
+
+      1.upto @colorbook_options[:color_count] do
+        color = {
+            name: read_string,
+            shortname: @colorbook_file.read(6),
+            color_values: []
+        }
+        puts @colorbook_options[:color_space_name]
+        bytes_len = AdobeColorBook.const_get("BYTES_#{@colorbook_options[:color_space_name]}")
+        3.times do
+          color[:color_values] << @colorbook_file.read(1).unpack('C')
+        end
+
+
+
+
+        puts color
+
+
+        @colorbook_options[:colors] << color
+      end
     end
   end
-
-  class ColorBookColor
-    attr_accessor :name, :shortname, :color_values
-    def initialize(color_opts = nil)
-      @name = color_opts[:name]
-      @shortname = color_opts[:shortname]
-      @color_values = color_opts[:color_values]
-    end
-
-    private
-
-    def read(file_ref)
-      fail("File is not valid") unless File.file? file_ref
-      @name = read_string file_ref
-      @shortname = file_ref.read(6).unpack("cccccc")
-
-    end
-  end
-
 end # end Module
 
 def testit
-AdobeColorBook::ColorBook.new "C:\\Users\\Owner\\Documents\\Contract\\Fischler\\acb2xml20\\PANTONE solid uncoated.acb"
+cb = AdobeColorBook::ColorBook.new "/home/gfsx/RubymineProjects/AdobeColorBook/test/PANTONE solid coated.acb"
 end
 
 testit
